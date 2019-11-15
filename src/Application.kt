71c140b138983b3aeaf436d90013882bc2edb0be
Safa Aman <sra68@drexel.edu>
2019-11-15 17:53:06 -0500

    Initial frontend, button triggers a modal

--- src/Application.kt
+++ src/Application.kt
@@ -1,17 +1,30 @@
 package com.blockchain
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.html.*
-import kotlinx.html.*
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.features.CORS
+import io.ktor.features.CallLogging
+import io.ktor.html.Placeholder
+import io.ktor.html.Template
+import io.ktor.html.insert
+import io.ktor.html.respondHtmlTemplate
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.content.*
+import io.ktor.request.path
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 import kotlinx.css.*
-import io.ktor.features.*
-import org.slf4j.event.*
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
+import kotlinx.html.*
+import org.slf4j.event.Level
+import java.io.File
+import io.ktor.html.insert as insert1
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -38,35 +51,30 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
+//        get("/styles.css") {
+//            call.respondCss {
+//                body {
+//                    backgroundColor = Color.red
+//                }
+//                p {
+//                    fontSize = 2.em
+//                }
+//                rule("p.myclass") {
+//                    color = Color.blue
+//                }
+//            }
+//        }
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get("/html-dsl") {
-            call.respondHtml {
-                body {
-                    h1 { +"HTML" }
-                    ul {
-                        for (n in 1..10) {
-                            li { +"$n" }
-                        }
-                    }
-                }
+            call.respondHtmlTemplate(MainTemplate()) {
+                content { +"Add a block" }
             }
         }
-
-        get("/styles.css") {
-            call.respondCss {
-                body {
-                    backgroundColor = Color.red
-                }
-                p {
-                    fontSize = 2.em
-                }
-                rule("p.myclass") {
-                    color = Color.blue
-                }
-            }
+        static("static") {
+            staticRootFolder = File("static")
+            files("css")
+            files("js")
+            file("image.png")
+            default("index.html")
         }
     }
 }
@@ -83,4 +91,46 @@ fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
 
 suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
     this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
+
+class MainTemplate (val modal: AddItemModalTemplate = AddItemModalTemplate()): Template<HTML> {
+    val content = Placeholder<HtmlBlockTag>()
+    override fun HTML.apply() {
+        head {
+            styleLink("/static/styles.css")
+            styleLink("https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css")
+            script(src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js") {}
+            script(src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js") {}
+            script(src = "/static/script.js") {}
+            title { +"SE 575 Blockchain Project" }
+        }
+        body {
+            this@apply.insert(modal) {}
+        }
+    }
+}
+
+class AddItemModalTemplate : Template<HTML> {
+    override fun HTML.apply() {
+        body {
+            a("#addItemModal", classes = "waves-effect waves-light btn modal-trigger") {
+                +"Add a block"
+            }
+            div(classes = "modal") {
+                id = "addItemModal"
+                div(classes = "modal-content") {
+                    h4 { +"Add a block" }
+                    textArea {
+                        id = "blockDataTextArea"
+                        classes += "materialize-textarea"
+                    }
+                    label { +"Block Data" }
+
+                }
+                div(classes = "modal-footer") {
+                    a("#!", classes = "modal-close waves-effect waves-green btn-flat") { +"Add" }
+                }
+            }
+        }
+    }
 }
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
 function message(msg) {
     alert(msg);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
