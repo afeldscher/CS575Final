8dd8419f3721199c7f399e507037c0a3d561d5e9
Safa Aman <sra68@drexel.edu>
2019-11-23 23:45:05 -0500

    Cleanup ktor application file

--- src/Application.kt
+++ src/Application.kt
@@ -7,15 +7,10 @@ import io.ktor.application.Application
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
 import io.ktor.features.CORS
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
-import io.ktor.html.Template
-import io.ktor.html.insert
-import io.ktor.html.respondHtmlTemplate
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
@@ -25,16 +20,12 @@ import io.ktor.request.path
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
 import kotlinx.css.CSSBuilder
 import kotlinx.html.*
 import org.slf4j.event.Level
-import java.io.File
 import java.text.DateFormat
-import org.apache.commons.codec.digest.DigestUtils
-
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -56,8 +47,6 @@ fun Application.module(testing: Boolean = false) {
         allowCredentials = true
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
-
-    val client = HttpClient(Apache) {}
 
     install(ContentNegotiation) {
         gson {
@@ -107,71 +96,4 @@ fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
 
 suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
     this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
-}
-
-
-//START HTML Templates
-class MainTemplate : Template<HTML> {
-    override fun HTML.apply() {
-        head {
-            styleLink("/static/styles.css")
-            styleLink("https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css")
-            script(src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js") {}
-            script(src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js") {}
-            script(src = "https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.min.js") {}
-            script(src = "/static/script.js") {}
-            title { +"SE 575 Blockchain Project" }
-        }
-        body {
-            nav {
-                div(classes = "nav-wrapper") {
-                    a("#", classes = "brand-logo") {
-                        +"Blockchain Simulator"
-                    }
-                }
-            }
-            div(classes = "container") {
-                insert(HeaderRowTemplate()) {}
-                div("row") {
-                    id = "blockWrapper"
-                }
-            }
-            insert(AddItemModalTemplate()) {}
-        }
-    }
-}
-
-class HeaderRowTemplate : Template<HtmlBlockTag> {
-    override fun HtmlBlockTag.apply() {
-        div(classes = " header-row row") {
-            div(classes = "col s4") {
-                a("#addItemModal", classes = "waves-effect waves-light btn modal-trigger") {
-                    +"Add a block"
-                }
-            }
-        }
-    }
-}
-
-class AddItemModalTemplate : Template<HtmlBlockTag> {
-    override fun HtmlBlockTag.apply() {
-        div(classes = "modal") {
-            id = "addItemModal"
-            div(classes = "modal-content") {
-                h4 { +"Add a block" }
-                div ("input-field add-block-textarea-wrapper"){
-                    textArea(classes = "materialize-textarea") {
-                        id = "blockDataTextArea"
-                    }
-                    label("active") {  +"Block Data" }
-                }
-            }
-            div(classes = "modal-footer") {
-                a("#!", classes = "modal-close waves-effect waves-green btn-flat") {
-                    onClick = "addBlock($('#blockDataTextArea').val())"
-                    +"Add"
-                }
-            }
-        }
-    }
 }
